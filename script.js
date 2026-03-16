6fc26b103fd890515a74698e9d7def997cdb6fe0
Malyanil Raj <malyanilraj@Malyanils-MacBook-Air.local>
2026-03-16 18:40:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// window.addEventListener("scroll",function(){
+// let cards=document.querySelectorAll(".card");
+
+// cards.forEach(card=>{
+// let position=card.getBoundingClientRect().top;
+
+// if(position<window.innerHeight){
+// card.classList.add("show");
+// }
+// });
+// });
