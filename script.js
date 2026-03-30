63ca0fd66e56ccae6d2688d4e50278e7573b1fcb
KashiPriya <priyakashi485@gmail.com>
2026-03-30 17:26:55 +0530

    Cosmetic style changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const toggle = document.getElementById("menuToggle");
 const menu = document.getElementById("navMenu");
-
+const btn = document.getElementById("nav-btn");
 toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
+    if (btn.style.display === "none") {
+        btn.style.display = "block"; // show
+    } else {
+        btn.style.display = "none"; // hide
+    }
 });
