c7f5338568f7f1e2fbc24826f2d694cccc094246
KashiPriya <priyakashi485@gmail.com>
2026-04-02 10:36:02 +0530

    image large

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const toggle = document.getElementById("menuToggle");
 const menu = document.getElementById("navMenu");
 const btn = document.getElementById("nav-btn");
+
+// LIGHTBOX FUNCTIONALITY
+
+const images = document.querySelectorAll(".gallery-item img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+
 toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
     if (btn.style.display === "none") {
@@ -20,4 +29,29 @@ faqs.forEach(btn => {
     answer.style.maxHeight = 
       answer.style.maxHeight ? null : answer.scrollHeight + "px";
   });
+});
+
+
+
+
+
+
+// OPEN IMAGE
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.classList.add("show");
+    lightboxImg.src = img.src;
+  });
+});
+
+// CLOSE BUTTON
+closeBtn.addEventListener("click", () => {
+  lightbox.classList.remove("show");
+});
+
+// CLICK OUTSIDE TO CLOSE
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== lightboxImg) {
+    lightbox.classList.remove("show");
+  }
 });
